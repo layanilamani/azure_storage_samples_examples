752ae5e1080a0d29e33a40e2834890ef02baa21e
layanilamani <layani.lamani@gmail.com>
2017-08-09 16:55:49 +0500

    first upload

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Table;
+
+
+namespace AzureStorageSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // UploadBlob();
+
+            AddInQue();
+        }
+
+        private static void UploadBlob()
+        {
+            var act = CloudStorageAccount.DevelopmentStorageAccount.CreateCloudBlobClient();
+
+            var container = act.GetContainerReference("aptech");
+
+            if (!container.Exists())
+            {
+                container.Create(BlobContainerPublicAccessType.Container);
+            }
+
+            var blob = container.GetBlockBlobReference("abc.docx");
+
+            blob.UploadFromFile(@"D:\mazhar\Enterprise Application Development using Azure.docx", System.IO.FileMode.Open);
+
+        }
+
+        private static void AddInQue()
+        {
+            var act = CloudStorageAccount.DevelopmentStorageAccount.CreateCloudQueueClient();
+
+            var que = act.GetQueueReference("aptech");
+
+            if (!que.Exists())
+            {
+                que.Create();
+            }
+
+            que.AddMessage(new Microsoft.WindowsAzure.Storage.Queue.CloudQueueMessage("salam"));            
+            
+
+        }
+    }
+}
