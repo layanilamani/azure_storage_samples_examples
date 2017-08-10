9a28a8b24cea7c2d434d034e36023debbf07b988
layanilamani <30865389+layanilamani@users.noreply.github.com>
2017-08-09 23:38:44 -0700

    added more functions.

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -12,29 +12,75 @@ using Microsoft.WindowsAzure.Storage.Table;
 
 namespace AzureStorageSamples
 {
+     public class Product : TableEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+
+        //public string Detail { get; set; }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-           // UploadBlob();
+            //  CreateBlobExample();
+            // CreateTableExample();
+
+            //SelectFromTable();
 
             AddInQue();
+
         }
 
-        private static void UploadBlob()
+        private static void SelectFromTable()
         {
-            var act = CloudStorageAccount.DevelopmentStorageAccount.CreateCloudBlobClient();
+            CloudStorageAccount act = CloudStorageAccount.Parse("UseDevelopmentStorage=true");
+            var client = act.CreateCloudTableClient();
 
-            var container = act.GetContainerReference("aptech");
+            var table = client.GetTableReference("Products2");
 
-            if (!container.Exists())
+            var q = new TableQuery<Product>();
+            q.Select(new string[] { "Id", "Name" });
+            q.Take(int.MaxValue);
+
+            var result = table.ExecuteQuery<Product>(q);
+
+            var cnt = result.Count();
+            foreach (var item in result)
             {
-                container.Create(BlobContainerPublicAccessType.Container);
+                Console.WriteLine(item.Id);
             }
 
-            var blob = container.GetBlockBlobReference("abc.docx");
+        }
 
-            blob.UploadFromFile(@"D:\mazhar\Enterprise Application Development using Azure.docx", System.IO.FileMode.Open);
+        private static void CreateTableExample()
+        {
+            CloudStorageAccount act = CloudStorageAccount.Parse("UseDevelopmentStorage=true");
+            var client = act.CreateCloudTableClient();
+
+            var table = client.GetTableReference("Products2");
+
+            if (!table.Exists())
+            {
+                table.Create();
+                //table.SetPermissions(new TablePermissions() { SharedAccessPolicies = })
+            }
+
+            var p = new Product();
+            p.RowKey = "Id";
+            p.Id = 3;
+            p.Name = "Samsung S1000";
+            p.Price = 105000;
+            p.PartitionKey = p.RowKey + p.Id;
+            //   p.Detail = "Some detail";
+
+            var op = TableOperation.Insert(p);
+
+            table.Execute(op);
 
         }
 
@@ -49,9 +95,27 @@ namespace AzureStorageSamples
                 que.Create();
             }
 
-            que.AddMessage(new Microsoft.WindowsAzure.Storage.Queue.CloudQueueMessage("salam"));            
-            
+            que.AddMessage(new Microsoft.WindowsAzure.Storage.Queue.CloudQueueMessage("salam"));
 
+        }
+
+        private static void CreateBlobExample()
+        {
+            CloudStorageAccount act = CloudStorageAccount.Parse("UseDevelopmentStorage=true");
+            var client = act.CreateCloudBlobClient();
+
+            var cont = client.GetContainerReference("aptech2");
+
+            if (!cont.Exists())
+            {
+                cont.Create();
+                cont.SetPermissions(new BlobContainerPermissions() { PublicAccess = BlobContainerPublicAccessType.Container });
+
+            }
+
+            var blob1 = cont.GetBlockBlobReference("abc.pdf");
+
+            blob1.UploadFromFile(@"E:\books\Building Applications in C INTL.pdf", FileMode.Open, null, null);
         }
     }
 }
